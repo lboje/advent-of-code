7a1993d4fa7c6c25a361c436154ba6c43e4e4501
Lucas Boje <lucasaboje@gmail.com>
2021-12-13 20:48:44 -0700

    day 1 part 2 complete

--- 2021/SonarSweep1/part1/main.py
+++ 2021/SonarSweep1/part1/main.py
@@ -1,31 +1,33 @@
 """
 Reads from an input.txt file. Input.txt is integers separated by line breaks.
-We keep track of how many times the input value increased and decreased from its previous value
+We keep track of how many times the input value increased from its previous value
 """
 import os
 
 
-def main():
-    decreasing = 0
-    increasing = 0
-
-    filePath = os.path.join(os.path.dirname(__file__), "input.txt")
-
-    #Note: I'm pretending that incorrect input files don't exist for advent of code
+#For advent of code purposes, the file is always there and correctly formatted
+def getFile(fileName):
+    filePath = os.path.join(os.path.dirname(__file__), fileName)
+    fileContents = []
     with open(filePath, "r") as file:
-        line = file.readline()
-        precedent = int(line.strip())
-        line = file.readline()
+        for line in file:
+            fileContents.append(int(line.strip()))
+    return fileContents
 
-        while line:
-            line = int(line.strip())
-            if line > precedent:
-                increasing = increasing + 1
-            else:
-                decreasing = decreasing + 1
-            precedent = line
-            line = file.readline()
-    print(increasing)
+
+#For a given array, iterate through and return number of times a value 
+#was greater than the one immediately preceding it
+def getIncrease(arr): 
+    increasing = 0
+    for i in range(len(arr)):
+        if i:
+            increasing += (1 if arr[i] > arr[i - 1] else 0)
+    return increasing
+
+
+def main():
+    fileContents = getFile("input.txt")    
+    print(getIncrease(fileContents))
 
 
 if __name__ == "__main__":
--- 2021/SonarSweep1/part2/main.py
+++ 2021/SonarSweep1/part2/main.py
@@ -1,33 +1,49 @@
 """
-Reads from an input.txt file. 
+Reads from an input.txt file. Input.txt is integers separated by line breaks.
+We create an array of the sums, in rolling groups of 3, of the values from the file.
+ex: arr =  [(file[0] + file[1] + file[2]), (file[1], file[2], file[3]), ...]
+Then, we determine how many times the values on that array increase as we iterate through it
+ex: arr[1] > arr[0]
 
-
-
+This is not the most efficient way of doing this, by any means.
 """
 import os
 
 
-def main():
-    decreasing = 0
-    increasing = 0
-
-    filePath = os.path.join(os.path.dirname(__file__), "input.txt")
-
-    #Note: I'm pretending that incorrect input files don't exist for advent of code
+#For advent of code purposes, the file is always there and correctly formatted
+#Read input file fileName within current directory, return input as array of integers
+def getFile(fileName):
+    filePath = os.path.join(os.path.dirname(__file__), fileName)
+    fileContents = []
     with open(filePath, "r") as file:
-        line = file.readline()
-        precedent = int(line.strip())
-        line = file.readline()
+        for line in file:
+            fileContents.append(int(line.strip()))
+    return fileContents
 
-        while line:
-            line = int(line.strip())
-            if line > precedent:
-                increasing = increasing + 1
-            else:
-                decreasing = decreasing + 1
-            precedent = line
-            line = file.readline()
-    print(increasing)
+
+#For an array of integers, return an array of the rolling sums of n items
+def rollingSums(arr, n):
+    sums = []
+    for i in range(len(arr) - (n - 1)):
+        nSum = sum(arr[i : (i + n)])
+        sums.append(nSum)
+    return sums 
+
+
+#For a given array, iterate through and return number of times a value 
+#was greater than the one immediately preceding it
+def getIncrease(arr): 
+    increasing = 0
+    for i in range(len(arr)):
+        if i:
+            increasing += (1 if arr[i] > arr[i - 1] else 0)
+    return increasing
+
+
+def main():
+    fileContents = getFile("input.txt") 
+    sums = rollingSums(fileContents, 3)  
+    print(getIncrease(sums))
 
 
 if __name__ == "__main__":
