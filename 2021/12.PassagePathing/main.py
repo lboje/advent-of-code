7cf70a87536a553833cb8d39ac450c96a4d67878
Lucas Boje <lucasaboje@gmail.com>
2021-12-23 21:58:57 -0700

    Completed day 12 pt 2

--- 2021/12.PassagePathing/main.py
+++ 2021/12.PassagePathing/main.py
@@ -10,17 +10,22 @@ A-end
 b-end
 
 This represents an undirected graph. 
-
-part 1: find all paths
+find all paths
+Completed 12/23/21
 """
 import os
-from collections import defaultdict
+from collections import defaultdict, Counter
 
 paths = 0
 #adjacency list for rooms
 cave = defaultdict(list)
-#big rooms can be entered more than once
+
+#big rooms can be entered however many times you want
 bigRooms = set()
+#goal rooms can be entered exactly once
+goalRooms = {'start', 'end'}
+
+
 
 #For advent of code purposes, the file is always there and correctly formatted
 def getFile(fileName):
@@ -36,6 +41,24 @@ def getFile(fileName):
             if end.isupper(): bigRooms.add(end)
     return
 
+"""
+You can enter a big room however many times you want
+You can enter goal rooms only once
+
+"""
+def canEnter(room, visited):
+    if room in bigRooms:
+        return True
+    if room in goalRooms:
+        return room not in visited
+    
+    #We only want to count small rooms
+    locationCount = Counter([location for location in visited if location not in bigRooms])
+    #If we have more than one of any small room, we don't want /more/
+    maxVal = 1 if locationCount.most_common(1)[0][1] > 1 else 2
+    return locationCount[room] < maxVal
+
+
 
 def searchPath(room, visited):
     global paths
@@ -44,7 +67,7 @@ def searchPath(room, visited):
         return
 
     for adjacent in cave[room]:
-        if adjacent not in visited or adjacent in bigRooms: 
+        if canEnter(adjacent, visited): 
             visited.append(adjacent)
             searchPath(adjacent, visited)
             visited.pop()
@@ -54,7 +77,7 @@ def searchPath(room, visited):
 def main():
     getFile("input.txt") 
     searchPath("start", ["start"])
-    print("Part one answer: {0}".format(paths))
+    print(paths)
     
 
 
