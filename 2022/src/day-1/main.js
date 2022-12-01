4035d3fbf6f971789a4f9c2e9dae6be140267b4b
Lucas Boje <lucasaboje@gmail.com>
2022-11-30 22:08:16 -0700

    Day 1 solution

--- 2022/src/day-1/main.js
+++ 2022/src/day-1/main.js
@@ -1,11 +1,36 @@
 "use strict";
 exports.__esModule = true;
 var express = require("express");
+var fs = require("fs");
 var app = express();
 var port = 3000;
+var fileContents = fs.readFileSync('src/day-1/input.txt').toString().split(/\n\s*\n/);
+var maxCalories = Number.MIN_SAFE_INTEGER;
+var secondBest = null;
+var bronzeMedal = null;
+for (var _i = 0, fileContents_1 = fileContents; _i < fileContents_1.length; _i++) {
+    var elf = fileContents_1[_i];
+    var totalCalories = 0;
+    for (var _a = 0, _b = elf.split('\n'); _a < _b.length; _a++) {
+        var calories = _b[_a];
+        totalCalories += +calories;
+    }
+    if (totalCalories > maxCalories) {
+        bronzeMedal = secondBest;
+        secondBest = maxCalories;
+        maxCalories = totalCalories;
+    }
+    else if (totalCalories > secondBest) {
+        bronzeMedal = secondBest;
+        secondBest = totalCalories;
+    }
+    else if (totalCalories > bronzeMedal) {
+        bronzeMedal = totalCalories;
+    }
+}
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.sendStatus(200);
 });
 app.listen(port, function () {
-    return console.log("Express is listening at http://localhost:".concat(port));
+    return console.log("Max: ".concat(maxCalories, "\nTop three: ").concat(maxCalories + secondBest + bronzeMedal));
 });
--- 2022/src/day-1/main.ts
+++ 2022/src/day-1/main.ts
@@ -1,11 +1,39 @@
 import express = require('express');
+import fs = require('fs');
+
 const app = express();
 const port = 3000;
 
+const fileContents = fs.readFileSync('src/day-1/input.txt').toString().split(/\n\s*\n/);
+
+
+let maxCalories = Number.MIN_SAFE_INTEGER;
+let secondBest = null;
+let bronzeMedal = null;
+
+for(let elf of fileContents) {
+    let totalCalories = 0;
+    for(let calories of elf.split('\n')) {
+        totalCalories += +calories;
+    }
+    if(totalCalories > maxCalories) {
+        bronzeMedal = secondBest;
+        secondBest = maxCalories;
+        maxCalories = totalCalories;
+    }
+    else if(totalCalories > secondBest) {
+        bronzeMedal = secondBest;
+        secondBest = totalCalories;
+    }
+    else if(totalCalories > bronzeMedal) {
+        bronzeMedal = totalCalories;
+    }
+}
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendStatus(200);
 });
 
 app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+  return console.log(`Max: ${maxCalories}\nTop three: ${maxCalories + secondBest + bronzeMedal}`);
 });
